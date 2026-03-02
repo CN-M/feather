d9ef236d62864651e756761ba6e66bcdfdeec3cb
C.N. <cn.mbhalati@gmail.com>
2026-03-02 11:02:03 +0200

    ♻️ Update base url for auth, update tsconfig stuff

--- apps/web/src/auth/server.ts
+++ apps/web/src/auth/server.ts
@@ -8,14 +8,14 @@ import { cache } from "react";
 // import { env } from "~/env";
 import { env } from "../env";
 
-const baseUrl = "http://localhost:3000";
+// const baseUrl = "http://localhost:3000";
 
-// const baseUrl =
-// 	env.VERCEL_ENV === "production"
-// 		? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
-// 		: env.VERCEL_ENV === "preview"
-// 			? `https://${env.VERCEL_URL}`
-// 			: "http://localhost:3000";
+const baseUrl =
+	env.VERCEL_ENV === "production"
+		? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
+		: env.VERCEL_ENV === "preview"
+			? `https://${env.VERCEL_URL}`
+			: "http://localhost:3000";
 
 export const auth = initAuth({
 	baseUrl,
